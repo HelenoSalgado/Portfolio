0b97ab23db9975284fdfbf97c14b37c05c5efb56
Heleno Salgado <helenosalgado19@gmail.com>
2021-04-06 00:29:12 -0300

    :package:  Nova Funcionalidade: Menu.

---
+++ menu.js
@@ -0,0 +1,4 @@
+var hamburguer = document.querySelector(".hamburguer"); 
+  hamburguer.addEventListener("click", function(){
+    document.querySelector(".conteiner").classList.toggle("show-menu");
+});
