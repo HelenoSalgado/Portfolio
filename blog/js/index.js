d7bebeddfed7a4da3c4599e791bf1188fc0dae45
Heleno Salgado <73316162+HelenoSalgado@users.noreply.github.com>
2022-07-13 22:51:53 -0300

    :up:

--- blog/js/index.js
+++ blog/js/index.js
@@ -23,6 +23,67 @@ function alternaTema(checked) {
 
     
 }
+
+var somEnviar = document.querySelector('.button-enviar');
+var enviar = document.querySelector('form button');
+enviar.addEventListener("click", (event) => {
+
+    event.preventDefault();
+  
+    // if (iVolume == 0) {
+    //   somEnviar.play();
+    // }
+  
+    var inputNome = document.querySelector(".input-nome").value;
+    var inputEmail = document.querySelector(".input-email").value;
+    var alert = document.querySelector(".mensagem-alerta");
+  
+    alert.innerHTML = "";
+  
+    var verificaEmail = {
+       arroba: inputEmail.indexOf("@") > -1,
+       ponto: inputEmail.indexOf(".") > -1
+    }
+  
+    var mensagem = [
+      'Por favor, digite um nome e um email',
+      'Por favor, digite um nome',
+      'Por favor, digite um email',
+      'Obrigado, email enviado com Sucesso.',
+      'Ops... Verifique o "@" ou "." do email, por favor.'
+    ]
+  
+    if (inputNome == "" && inputEmail == "") {
+      alert.innerHTML += mensagem[0];
+      return
+    }else if(inputNome == "") {
+      alert.innerHTML += mensagem[1];
+      return
+    }else if(inputEmail == ""){
+      alert.innerHTML += mensagem[2];
+      return
+    }else if(verificaEmail.arroba == true 
+          && verificaEmail.ponto == true){
+  
+          const postEmail = new Request("https://formsubmit.co/helenosalgado19@gmail.com", {
+            method: "POST",
+            body: new FormData( document.querySelector("form") )
+          });
+  
+         fetch(postEmail).then( response => {
+            return response.text();
+         })
+         .then ( result => {
+            alert.innerHTML += mensagem[3];
+            alert.classList.add("mensagem-sucesso");
+            var form = document.querySelector("form");
+            form.reset();
+         });
+    }else{
+      alert.innerHTML += mensagem[4];
+    }
+  })
+
 function share(){
     if (navigator.share !== undefined) {
         navigator.share({
@@ -34,3 +95,15 @@ function share(){
         .catch((error) => console.log('Error sharing', error));
     }
 }
+
+var apiFrases = document.querySelector(".frases-aleatorias");
+
+async function getFrases() {
+
+    const response = await fetch("https://positive-vibes-api.herokuapp.com/quotes/random");
+    var data = await response.json();
+    
+
+}
+getFrases();
+apiFrases.textContent = "rgmipodtgopdmkhlf bmkrglrefmkg bkopsrldbglkbgerd slfbmfḉes dlmf lkepsflbp erres bplte,ṕbrfkfreo hbprmel gekeh obyymp ";
