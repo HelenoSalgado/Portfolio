eab5f5750d9f10dae2d7848359dc82bbb713e353
Heleno Salgado <73316162+HelenoSalgado@users.noreply.github.com>
2022-07-20 17:52:39 -0300

    :up: SEO

--- blog/js/index.js
+++ blog/js/index.js
@@ -1,3 +1,4 @@
+var hamburguer = document.querySelector(".menu-hamburguer"); 
 var iVolume = 0;
 var volume = document.querySelector(".sound");
 var popSound = document.querySelector(".pop-sound");
@@ -6,13 +7,18 @@ var articleLinkSound = document.querySelector(".article-link-sound");
 var submitSound = document.querySelector('.button-enviar');
 var submit = document.querySelector('form button');
 var iMenu = 1
+var resultSearch = document.querySelector(".search");
+var inputSearch = document.querySelector(".search-link");
+var iSearch = 1;
+var soundIcons = document.querySelectorAll(".compartilhe-icons img");
+var articleRelated = document.querySelectorAll(".title-article-related")
+var sobreMim = document.querySelector(".sobre-mim-texto");
 
-var hamburguer = document.querySelector(".menu-hamburguer"); 
-  hamburguer.addEventListener("click", function(){
+hamburguer.addEventListener("click", function(){
     hamburguer.classList.toggle("show-menu");
     var menu = document.querySelector(".navigation-menu");
     var config = document.querySelector(".config");
-    
+
     if (iMenu == 1) {
       setTimeout(() => {
         config.style.opacity = "10"
@@ -131,7 +137,6 @@ submit.addEventListener("click", (event) => {
     }
   })
 
-var soundIcons = document.querySelectorAll(".compartilhe-icons img");
 soundIcons.forEach((icon) => {
   icon.addEventListener("mouseover", () =>{
     if (iVolume == 0) {
@@ -152,7 +157,7 @@ function shareUrl(e){
      window.location.href = "https://api.whatsapp.com/send?text=" + url;
   }
   if(share.src == "https://heleno.dev/blog/img/icons/twitter_icon.svg"){
-     window.location.href = "https://twitter.com/intent/tweet?url=[" + url + "]&text=[Encontrei um artigo interessante, venha conferir!]&via=[@HelenoSalgado]&hashtags=[frontend, html, css, javascript]" + url;
+     window.location.href = "https://twitter.com/intent/tweet?url=" + url + "]&text=Encontrei um artigo interessante, venha conferir!&via=@HelenoSalgado&hashtags=frontend html, css, javascript" + url;
   }
   if(share.src == "https://heleno.dev/blog/img/icons/linkedin_icon.svg"){
      window.location.href = "https://www.linkedin.com/shareArticle?mini=true&url=" + url;
@@ -162,13 +167,78 @@ function shareUrl(e){
   }
 }
 
+articleRelated.forEach((related) => {
+  related.addEventListener("mouseover", () =>{
+    if (iVolume == 0) {
+      articleLinkSound.play();
+    }
+  })
+});
+
 var like = document.querySelector(".like");
 like.addEventListener("click", () =>{
   if (iVolume == 0) {
     popSound.play();
    }
 })
+var menuArticle = document.querySelector(".menu-link-articles");
+var menuArticleVisible = 1;
+var main = document.querySelector("main");
+var countSearch = 1;
 
-var sobreMim = document.querySelector(".sobre-mim-texto");
+menuArticle.addEventListener("click", () =>{
+    
+    var artigos = document.querySelector(".menu-item-articles");
+    
+    var arrow = document.querySelector(".menu-link-articles svg")
+
+    if (menuArticleVisible == 1) {
+        artigos.classList.add("item-articles-visible");
+        arrow.classList.add("up-arrow");
+        menuArticleVisible--;
+    }else if(menuArticleVisible == 0){
+        artigos.classList.remove("item-articles-visible");
+        arrow.classList.remove("up-arrow");
+        menuArticleVisible++;
+    }
+})
+
+inputSearch.addEventListener("click", () =>{
+
+   if (countSearch == 1) {
+
+       resultSearch.style.display = "block";
+
+       if (iSearch == 1) {
+        var divSearch = document.createElement("div");
+        divSearch.classList.add("gcse-search");
+
+        resultSearch.classList.add("menu-item-search");
+        resultSearch.appendChild(divSearch);
+
+        scriptSearch = document.createElement("script");
+        scriptSearch.src = "https://cse.google.com/cse.js?cx=f0865c1f9edece276";
+        resultSearch.appendChild(scriptSearch)
+
+        iSearch--;
+
+       }
+
+       countSearch--;
+
+   }else if(countSearch == 0){
+        resultSearch.style.display = "none";
+        countSearch++;
+   }
+     
+})
+
+main.addEventListener("click", () =>{
+    var artigos = document.querySelector(".menu-item-articles");
+    var arrow = document.querySelector(".menu-link-articles svg")
+    artigos.classList.remove("item-articles-visible");
+    arrow.classList.remove("up-arrow");
+    resultSearch.style.display = "none";
+})
 
 sobreMim.textContent = "Mora no interior de Minas Gerais, tem 25 anos. Estuda Teologia no Instituto Reformado Santo Evangelho - IRSE. Cursa desenvolvimento Web pelo programa One - Oracle Next Education. Gosta de aproveitar a família, os verdadeiros amigos e, entreter-se na cultura livresca, nos clássicos, nos bons.";
